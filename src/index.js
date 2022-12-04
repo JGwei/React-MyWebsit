14506635fc96de9bf7321c84ba0554d4577f660d
GuangWei <yyopop10231@gmail.com>
2022-12-04 20:39:19 +0800

    update 2022-12-04 20:39:19

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import Contact from './views/contact/contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       {/* 頭部 */}
       <HeadNav/>
       <div className="App">
